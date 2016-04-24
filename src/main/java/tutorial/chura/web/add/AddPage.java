c5836adaa7db8e52368f2b18622b059bba039e32
Keita sano <metheny@jp.bigplanet.com>
2016-04-24 18:22:46 +0900

    htmlとPageを配置直後の状態。

---
+++ src/main/java/tutorial/chura/web/add/AddPage.java
@@ -0,0 +1,21 @@
+package tutorial.chura.web.add;
+
+public class AddPage {
+	
+	public Integer arg1;
+	public Integer arg2;
+	public Integer result;
+
+	public Class<?> doCalculate() {
+		return null;
+	}
+
+	public Class<?> initialize() {
+		return null;
+	}
+
+	public Class<?> prerender() {
+		return null;
+	}
+
+}
